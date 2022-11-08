8da77f7ba61e7dcef42d3d098bc0017c9409f578
NaNeG <kolosovg02@hotmail.com>
2022-11-08 19:27:14 +0500

    test fix

--- project/src/components/app/app.test.tsx
+++ project/src/components/app/app.test.tsx
@@ -1,8 +1,14 @@
-import {render} from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './app';
 import { films } from '../../mocks/films';
 import reviews from '../../mocks/reviews';
+import { Provider } from 'react-redux';
+import { store } from '../../store';
 
 test('Renders app-component', () => {
-  render(<App mockFilms={films} reviews={reviews}/>);
+  render(
+    <Provider store={store}>
+      <App mockFilms={films} reviews={reviews} />
+    </Provider>
+  );
 });
